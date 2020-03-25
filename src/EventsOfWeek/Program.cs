e55178184e3349b39792e9447187cdbed74659a7
Artur Yaroshinski <62154380+arturyaroshinski@users.noreply.github.com>
2020-03-25 12:54:43 +0300

    feat: add new project "EventsOfWeek"

---
+++ src/EventsOfWeek/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace EventsOfWeek
+{
+    class Program
+    {
+        enum Week
+        {
+            Sunday = 0,
+            Monday = 1,
+            Tuesday = 2,
+            Wednesday = 3,
+            Thursday = 4,
+            Friday = 5,
+            Saturday = 6
+        }
+        static string[] arrayWithEvents = new string[7];
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Hello, dear friend.");
+                Console.WriteLine("You can select the day by entering its name or number.");
+                Console.WriteLine("Example: 'Monday', 'mon', '1'(Remember that sunday - 0, monday - 1 etc.).");
+                Console.WriteLine("Good luck!");
+                Console.WriteLine("**************************************************************************");
+                SelectDay();
+                Console.WriteLine("Input 'q' for exit, any other input will restart app");
+                if (Console.ReadLine().Equals("q"))
+                {
+                    return;
+                }
+                Console.Clear();
+            }
+        }
+        static void SelectDay()
+        {
+            string userInput = Console.ReadLine();
+            switch (userInput.ToLower())
+            {
+                case "sun":
+                case "sunday":
+                case "0":
+                    Console.WriteLine("U choose sunday.");
+                    AddEvent(Convert.ToInt32(Week.Sunday));
+                    break;
+                case "mon":
+                case "monday":
+                case "1":
+                    Console.WriteLine("U choose monday.");
+                    AddEvent(Convert.ToInt32(Week.Monday));
+                    break;
+                case "tue":
+                case "tuesday":
+                case "2":
+                    Console.WriteLine("U choose tuesday.");
+                    AddEvent(Convert.ToInt32(Week.Tuesday));
+                    break;
+                case "wed":
+                case "wednesday":
+                case "3":
+                    Console.WriteLine("U choose wednesday.");
+                    AddEvent(Convert.ToInt32(Week.Wednesday));
+                    break;
+                case "thu":
+                case "thurday":
+                case "4":
+                    Console.WriteLine("U choose thursday.");
+                    AddEvent(Convert.ToInt32(Week.Thursday));
+                    break;
+                case "fri":
+                case "friday":
+                case "5":
+                    Console.WriteLine("U choose friday.");
+                    AddEvent(Convert.ToInt32(Week.Friday));
+                    break;
+                case "sat":
+                case "saturday":
+                case "6":
+                    Console.WriteLine("U choose saturday.");
+                    AddEvent(Convert.ToInt32(Week.Saturday));
+                    break;
+                default:
+                    Console.WriteLine("Invalid input, try again.");
+                    SelectDay();
+                    break;
+            }
+        }
+        static void AddEvent(int day)
+        {
+            if (string.IsNullOrWhiteSpace(arrayWithEvents[day]))
+            {
+                Console.Write("Input event:");
+                arrayWithEvents[day] = Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("U already have event at this day: " + arrayWithEvents[day]);
+            }
+        }
+    }
+}
+
