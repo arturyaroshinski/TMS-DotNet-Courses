5acd16e1d9b1dda7f582ecbc97e3cd5a10dd5213
Artur Yaroshinski <62154380+arturyaroshinski@users.noreply.github.com>
2020-04-09 14:02:17 +0300

    feat: add "SimpleATM" project

---
+++ src/SimpleATM/Bank/Bank.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace BankLib
+{
+    public class Bank
+    {
+        public static void Menu(Client client)
+        {
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                Console.WriteLine("Menu: ");
+                Console.WriteLine("[1] Put money.\n[2] Take money.\n[3] Check balance.\n[4] Quit app.");
+                Console.ResetColor();
+                Console.WriteLine("Choose option by writing its number.");
+
+                string menuOption = Console.ReadLine();
+
+                switch (menuOption)
+                {
+                    case "1":
+                        Console.WriteLine("Enter amout:");
+                        decimal.TryParse(Console.ReadLine(), out decimal putMoney);
+                        client.Put(putMoney);
+                        break;
+                    case "2":
+                        Console.WriteLine("Enter amount");
+                        decimal.TryParse(Console.ReadLine(), out decimal takeMoney);
+                        client.Put(takeMoney);
+                        break;
+                    case "3":
+                        client.Balance();
+                        break;
+                    case "4":
+                        return;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Invalid input, try again.");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ src/SimpleATM/Bank/Client.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BankLib
+{
+    public class Client
+    {
+        public event Action<string> Notify;
+
+        private decimal _currBalance;
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public void Put(decimal money)
+        {
+            _currBalance += money;
+            Notify?.Invoke($"You put {money}$, your current balance: {_currBalance}$");
+        }
+        public void Take(decimal money)
+        {
+            _currBalance -= money;
+            Notify?.Invoke($"You take { money}$, your current balance: { _currBalance}$");
+        }
+        public void Balance()
+        {
+            Console.WriteLine($"Your current balance: {_currBalance}.");
+        }
+        public Client() { }
+        public Client(string name, decimal balance)
+        {
+            Name = name;
+            _currBalance += balance;
+        }
+    }
+}
